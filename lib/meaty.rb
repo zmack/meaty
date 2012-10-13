5cfef1a1ba26d5516852201f3c9b130b33e5d6eb
Andrei Bocan <zmaxor@gmail.com>
2012-10-13 17:04:52 +0300

    Synchronization

--- lib/meaty.rb
+++ lib/meaty.rb
@@ -1,27 +1,43 @@
 require_relative "meaty/version.rb"
 require_relative "meaty/error.rb"
 require "logger"
+require "faraday"
 
 module Meaty
   class Logger < ::Logger
-    def initialize(logdev, shift_age = 0, shift_size = 1048576)
+    attr_reader :options
+
+    def initialize(logdev, shift_age = 0, shift_size = 1048576, options = {})
+      verify_correct_options!(options)
+
       if logdev.is_a? String
         super
+      elsif logdev == :meaty
+        super(nil)
+        @logdev = LogDevice.new(options)
       else
         super(nil)
+        @logdev = logdev
       end
 
-      @logdev = logdev
       @formatter = Formatter.new
     end
 
     def flush; end
+
+  private
+    def verify_correct_options!(options)
+      return true if options.empty? || options.has_key?(:api_key)
+
+      raise ArgumentError
+    end
   end
 
   class Formatter
     def call(severity, time, progname, msg)
       {
         :severity => severity,
+        :pid => $$,
         :time => time,
         :progname => progname,
         :msg => msg
@@ -47,9 +63,30 @@ module Meaty
     end
   end
 
-  def Synchronizer
-    def synchronize(data)
+  class Synchronizer
+    attr_accessor :connection
 
+    def initialize(api_key, options)
+      url = options.fetch(:url)
+      @api_key = api_key
+      @connection = Faraday.new(:url => url)
+    end
+
+    def synchronize(data)
+      connection.post do |request|
+        request.path = '/api/messages'
+        request.headers['API_KEY'] = api_key
+        request.headers['Content-Type'] = data.content_type
+        request.body = data.serialized
+      end
+      { :successful => true, :response => connection.response }
+    rescue Faraday::Error::ClientError => e
+      { :successful => false, :exception => e }
+    end
+
+    private
+    def api_key
+      @api_key
     end
   end
 end
--- test/meaty_test.rb
+++ test/meaty_test.rb
@@ -1,4 +1,5 @@
 require File.join(File.dirname(File.expand_path(__FILE__)), '/test_helper.rb')
+require 'ostruct'
 
 describe Meaty::Logger do
   it "should do some things" do
@@ -33,6 +34,20 @@ describe Meaty::LogDevice do
     @logger.error "Foobar"
 
     @device.buffer.length.must_equal 1
+  end
+end
+
+describe Meaty::Synchronizer do
+  before do
+    @sync = Meaty::Synchronizer.new('1234', {
+      :url => "http://test.bar"
+    })
+    @data = OpenStruct.new(:content_type => 'application/json', :serialized_data => '{ foo: "bar" }')
+  end
+
+  it "should set :successful as the marker of the outcome of the request" do
+    result = @sync.synchronize(@data)
+    result[:successful].must_equal false
   end
 end
 
